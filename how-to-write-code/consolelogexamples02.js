f75dfbe61e21a9ed1ca3b85689568d37c4e96b4d
Dovrannazar1990 <adovrannazar@yahoo.com>
2023-08-20 21:03:41 -0500

    Pre-work was done by Dovrannazar

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,3 +1,10 @@
-console.log("Some Programming Languages:");
+console.log("Some Programming Languages:"); // Some Programming Languages:
 
 console.log("Python\nJavaScript\nJava\nC#\nSwift");
+/*
+Python
+JavaScript
+Java
+C#
+Swift
+*/
